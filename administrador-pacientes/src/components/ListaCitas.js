49e11249ed11c0c84c92c8820cb0b35934f2a363
CrisRod <crodriguez85@gmail.com>
2019-09-03 18:14:17 -0400

    Props paso eliminar a lista

--- administrador-pacientes/src/components/ListaCitas.js
+++ administrador-pacientes/src/components/ListaCitas.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cita from './Cita';
 
-const ListaCitas = ({citas}) => {
+const ListaCitas = ({citas, eliminarCita}) => {
     return ( 
         <div className="card mt-2 py-5">
             <div className="card-body">
