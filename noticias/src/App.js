8a739415afa2c4cae3ef9ed1db070330b4e9ef0d
CrisRod <crodriguez85@gmail.com>
2019-09-03 18:50:38 -0400

    Api fetch async await console.log

--- noticias/src/App.js
+++ noticias/src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 
 class App extends Component {
   state = {  }
+
+  async componentDidMount() {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a7a5d8e389ee4438a38d3407b4f66b85`;
+    const respuesta = await fetch(url);
+    const  noticias = await respuesta.json();
+    console.log(noticias)
+  }
+
+
+
   render() { 
     return ( 
       <h1>Noticias React</h1>
