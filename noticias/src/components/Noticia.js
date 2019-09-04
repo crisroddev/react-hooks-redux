0d7b52762748307290941d3f53a1e85c62d61e3e
CrisRod <crodriguez85@gmail.com>
2019-09-04 09:22:22 -0400

    Terminando el componente noticia

--- noticias/src/components/Noticia.js
+++ noticias/src/components/Noticia.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
-const Noticas = () => {
+const Noticas = ({noticia}) => {
+    
+    // Extraer Datos
+    const {urlToImage, url, title, descritpion } = noticia;
+    
     return ( 
-        <h1>Desde Noticias</h1>
+        <div className="col s12 m6 l4">
+            <div className="card">
+                <div className="card-image">
+                    <img src={urlToImage} alt={title}/>
+                </div>
+                <div className="card-content">
+                    <h3>{title}</h3>
+                    <p>{descritpion}</p>
+                </div>
+                <div className="card-action">
+                    <a 
+                        href={url} 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="waves-effect waves-light btn">
+                            Ver Noticia
+                        </a>
+                </div>
+            </div>
+        </div>    
      );
 }
  
