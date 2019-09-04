605f439d06cf248b449afa99250b31fd3180e2f5
CrisRod <crodriguez85@gmail.com>
2019-09-04 10:23:56 -0400

    Api Key eventBrite

--- eventos/src/context/CategoriasContext.js
+++ eventos/src/context/CategoriasContext.js
@@ -5,6 +5,8 @@ const CategoriasContext = React.createContext();
 export const CategoriasConsumer = CategoriasContext.Consumer
 
 class CategoriasProvider extends Component {
+    token = 'PZHE6E254BGLDF6FCD';
+
     state = {  }
     render() { 
         return (  );
