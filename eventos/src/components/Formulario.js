ba6c2365cb7ce79f892ad7ec04d49113a541a55b
CrisRod <crodriguez85@gmail.com>
2019-09-04 10:48:35 -0400

    Form import Consumer

--- eventos/src/components/Formulario.js
+++ eventos/src/components/Formulario.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { CategoriasConsumer } from '../context/CategoriasContext';
 
 class Formulario extends Component {
     state = { 
