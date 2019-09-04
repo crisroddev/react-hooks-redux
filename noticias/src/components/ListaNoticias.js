57842beda46e5bc7b627c60a1a752dc3d57379ac
CrisRod <crodriguez85@gmail.com>
2019-09-04 09:13:04 -0400

    Llamado las noticias

--- noticias/src/components/ListaNoticias.js
+++ noticias/src/components/ListaNoticias.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Noticia from './Noticia';
 
 const ListaNoticias = ({noticias}) => (
     <div className="row">
---
+++ noticias/src/components/Noticia.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Noticas = () => {
+    return ( 
+        <h1>Desde Noticias</h1>
+     );
+}
+ 
+export default Noticas;
