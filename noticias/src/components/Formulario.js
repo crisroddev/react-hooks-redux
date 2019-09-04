234dd3f4437b8a4a8f0f00e3085ed4b7333f90b7
CrisRod <crodriguez85@gmail.com>
2019-09-04 09:31:06 -0400

    Pasando options al form

--- noticias/src/components/Formulario.js
+++ noticias/src/components/Formulario.js
@@ -10,10 +10,19 @@ class Formulario extends Component {
                 <div className="col s12 m8 offset-2">
                     <form>
                         <h2>Encuentra Noticias por Categoria</h2>
+                        <div className="input-field col s12 m8">
+                            <select>
+                                <option value="general">General</option>
+                                <option value="business">Negocios</option>
+                                <option value="entertainment">Entertainment</option>
+                                <option value="health">Salud</option>
+                                <option value="science">Ciencia</option>
+                                <option value="sports">Deportes</option>
+                                <option value="technology">Tecnologias</option>
+                            </select>
+                        </div>
                     </form>
-
                 </div>
-
            </div> 
          );
     }
