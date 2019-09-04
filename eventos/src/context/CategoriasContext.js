351e93d024747657346929f978b7889e4c868616
CrisRod <crodriguez85@gmail.com>
2019-09-04 10:20:25 -0400

    Creando el context y consumer

---
+++ eventos/src/context/CategoriasContext.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+// Crear Context
+const CategoriasContext = React.createContext();
+export const CategoriasConsumer = CategoriasContext.Consumer
+
+class CategoriasProvider extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default CategoriasProvider;
