5b743c1a786def1b3831a6026f54f0d6c1a52805
CrisRod <crodriguez85@gmail.com>
2019-09-04 13:45:34 -0400

    pasando los values en form

--- eventos/src/components/Formulario.js
+++ eventos/src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CategoriasConsumer } from '../context/CategoriasContext';
+import { EventosConsumer } from '../context/EventosContext';
 
 class Formulario extends Component {
     state = { 
@@ -17,55 +18,69 @@ class Formulario extends Component {
 
     render() { 
         return ( 
-            <form>
-                <fieldset className="uk-fieldset uk-margin">
-                    <legend className="uk-legend uk-text-center">
-                        Busca tu evento por Nombre o Categoria
-                    </legend>
-                </fieldset>
 
-                <div className="ok-column-1-3@m uk-margin">
-                    <div className="uk-margin" uk-margin="true">
-                        <input 
-                            name="nombre"
-                            className="uk-input"
-                            type="text"
-                            placeholder="Nombre de Evento o Ciudad"
-                            onChange={this.handleOnChange}
-                        />
-                    </div>
-                    <div className="uk-margin" uk-margin="true">
-                        <select
-                            className="uk-select"
-                            name="categoria"
-                            onChange={this.handleOnChange}>
-                            <option value="">---Selecciona la Categoria---</option>
-                            <CategoriasConsumer>
-                                {(value) => {
-                                    return(
-                                        value.categorias.map(categoria => (
-                                            <option 
-                                                key={categoria.id}
-                                                value={categoria.id}
-                                                data-uk-form-select>
-                                                {categoria.name_localized}
-                                            </option>
-                                        ))
-                                    )
-                                }}
-                            </CategoriasConsumer>
+            <EventosConsumer>
+            {(value) => {
+                {/* console.log(value) */}
+                return(
+                <form
+                    onSubmit={e => {
+                        e.preventDefault();
+                        value.obtenerEventos(this.state)
+                    }}>
+                    <fieldset className="uk-fieldset uk-margin">
+                        <legend className="uk-legend uk-text-center">
+                            Busca tu evento por Nombre o Categoria
+                        </legend>
+                    </fieldset>
 
-                        </select>
+                    <div className="ok-column-1-3@m uk-margin">
+                        <div className="uk-margin" uk-margin="true">
+                            <input 
+                                name="nombre"
+                                className="uk-input"
+                                type="text"
+                                placeholder="Nombre de Evento o Ciudad"
+                                onChange={this.handleOnChange}
+                            />
+                        </div>
+                        <div className="uk-margin" uk-margin="true">
+                            <select
+                                className="uk-select"
+                                name="categoria"
+                                onChange={this.handleOnChange}>
+                                <option value="">---Selecciona la Categoria---</option>
+                                <CategoriasConsumer>
+                                    {(value) => {
+                                        return(
+                                            value.categorias.map(categoria => (
+                                                <option 
+                                                    key={categoria.id}
+                                                    value={categoria.id}
+                                                    data-uk-form-select>
+                                                    {categoria.name_localized}
+                                                </option>
+                                            ))
+                                        )
+                                    }}
+                                </CategoriasConsumer>
+
+                            </select>
+                        </div>
+                        <div className="uk-margin" uk-margin="true">
+                            <input
+                                type="submit"
+                                className="uk-button uk-button-danger"
+                                value="Buscar Eventos"
+                            />
+                        </div>
                     </div>
-                    <div className="uk-margin" uk-margin="true">
-                        <input
-                            type="submit"
-                            className="uk-button uk-button-danger"
-                            value="Buscar Eventos"
-                        />
-                    </div>
-                </div>
-            </form>
+                </form>
+                )
+            }}
+
+            </EventosConsumer>
+
          );
     }
 }
