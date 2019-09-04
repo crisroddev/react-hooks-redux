be8f5698179574f6dacd5d028318698563689f76
CrisRod <crodriguez85@gmail.com>
2019-09-04 09:27:11 -0400

    Empezando el form

---
+++ noticias/src/components/Formulario.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Formulario extends Component {
+    state = { 
+
+     }
+    render() { 
+        return ( 
+           <div className="buscador row">
+                <div className="col s12 m8 offset-2">
+                    <form>
+                        <h2>Encuentra Noticias por Categoria</h2>
+                    </form>
+
+                </div>
+
+           </div> 
+         );
+    }
+}
+ 
+export default Formulario;
