04fd2fb13b6e4c780600126eb9ac99385aa6d96c
CrisRod <crodriguez85@gmail.com>
2019-09-05 16:14:42 -0400

    form

---
+++ clima-hooks/src/components/Formulario.js
@@ -0,0 +1,46 @@
+import React from 'react'
+ 
+const Formulario = () => {
+
+    const handleChange = e => {
+        // Cambiar el State
+    }
+
+    return ( 
+        <form>
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad:</label>
+            </div>
+            <div className="input-field col s12">
+                <select 
+                    onChange={handleChange} 
+                    name="pais">
+                    <option value="">Selecciona un Pais</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="CL">Chile</option>
+                    <option value="MX">Mexico</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Peru</option>
+                </select>
+            </div>
+            <div className="input-field col s12">
+                <input
+                    type="submir"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                    value="Buscar Clima"
+                />
+            </div>
+        </form>
+     );
+}
+ 
+export default Formulario;
