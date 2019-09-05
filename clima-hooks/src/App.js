89075edaccb5c4f7ca9dd2388f95ed106981eb1b
CrisRod <crodriguez85@gmail.com>
2019-09-05 16:41:47 -0400

    props metodo datosConsulta

--- clima-hooks/src/App.js
+++ clima-hooks/src/App.js
@@ -3,6 +3,12 @@ import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const datosConsulta = datos => {
+    console.log(datos)
+  }
+
+
   return (
     <div className="App">
       <Header
@@ -12,7 +18,9 @@ function App() {
       <div className="contenedor-form">
         <div className="row">
           <div className="col s12 m6">
-            <Formulario/>
+            <Formulario
+              datosConsulta={datosConsulta}
+            />
           </div>
         </div>
       </div>
