b1544b17d49b9502a3420f7ca0f7b7363dd1a223
CrisRod <crodriguez85@gmail.com>
2019-09-05 15:39:06 -0400

    Header

---
+++ clima-hooks/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+   return (
+       <nav>
+            <div className="nav-wrapper light-blue darken-2">
+                <a href="#!" className="brand-logo">{titulo}</a>
+            </div>
+       </nav>
+   ) 
+}
+ 
+export default Header;
