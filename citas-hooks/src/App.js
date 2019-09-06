4e28338214bdc986b2b8db41a1c206240cdff9d9
CrisRod <crodriguez85@gmail.com>
2019-09-06 10:30:35 -0400

    partiendo form

--- citas-hooks/src/App.js
+++ citas-hooks/src/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+
+// Components
+import Formulario from './components/Formulario';
 
 function App() {
   // UseState
   const [ citas, setCita ] = useState([]);
   return(
-    <h1>Hola</h1>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario/>
+          </div>
+          <div className="one-half column">
+
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
+
   )
 
 }
