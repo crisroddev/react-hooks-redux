cbdec8214cf6df150d52417dc01bfa07d144815b
CrisRod <crodriguez85@gmail.com>
2019-09-06 17:56:44 -0400

    Pregunta en App

--- presupuesto-hooks/src/App.js
+++ presupuesto-hooks/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+// Components
+import Pregunta from './components/Pregunta';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Gasto Semanal</h1>
+
+        <Pregunta/>
       </header>
     </div>
   );
