1fb63d029235065734c5c5b77ccadb01d1b09ccd
CrisRod <crodriguez85@gmail.com>
2019-09-06 10:35:02 -0400

    Form style

--- citas-hooks/src/components/Formulario.js
+++ citas-hooks/src/components/Formulario.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Formulario = () => {
     return (
-        <h1>Hola</h1>
+        <Fragment>
+            <h2>Crear Cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                    <input 
+                        type="text" 
+                        name="mascota"
+                        className="u-full-width" 
+                        placeholder="Nombre Mascota" 
+                    />
+                    <label>Nombre Dueño</label>
+                    <input 
+                        type="text" 
+                        name="propietario"
+                        className="u-full-width"  
+                        placeholder="Nombre Dueño de la Mascota" 
+                    />
+                    <label>Fecha</label>
+                    <input 
+                        type="date" 
+                        className="u-full-width"
+                        name="fecha"
+                    />               
+                    <label>Hora</label>
+                    <input 
+                        type="time" 
+                        className="u-full-width"
+                        name="hora" 
+                    />
+                    <label>Sintomas</label>
+                    <textarea 
+                        className="u-full-width"
+                        name="sintomas"
+                    ></textarea>
+                    <button type="submit" className="button-primary u-full-width">Agregar</button>
+                </form>
+        </Fragment>
+
       );
 }
  
