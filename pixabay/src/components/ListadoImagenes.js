dc272f3f317ea5f79dfd2b271ffbf0c6448922c9
CrisRod <crodriguez85@gmail.com>
2019-09-09 18:22:59 -0300

    Comp Listadoimagenes

---
+++ pixabay/src/components/ListadoImagenes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoImagenes = () => {
+    return ( 
+        <div className="col-12 p-5 row">
+            
+        </div>
+     );
+}
+ 
+export default ListadoImagenes;
