6c7f198098c7a37c2dba106b168c5443866d9aab
CrisRod <crodriguez85@gmail.com>
2019-09-09 15:06:18 -0300

    async await useEffect fetch api

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [ frase, obtenerFrase ] = useState({});
+
+
+  // Consulta a RestAPI
+  useEffect(() => {
+    async () => {
+      const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/version1/quotes')
+      console.log(resultado);
+    }
+
+  })
+
+
   return(
     <p>Hola</p>
   )
