c7e2c6c0651964d6b0e7829d7f05a766e277fbb1
CrisRod <crodriguez85@gmail.com>
2019-09-09 15:17:27 -0300

    separando func para API y func dentro de useeffect

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -5,13 +5,18 @@ function App() {
 
   const [ frase, obtenerFrase ] = useState({});
 
+  const consultarApi = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    console.log(resultado)
+}
+
 
   // Consulta a RestAPI
   useEffect(
-    async () => {
-      const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
-      console.log(resultado)
-  })
+    () => {
+      consultarApi()
+    }
+  )
 
 
   return(
