fbb4bb359397203ab3a7f6e5af0eadbef5a7d2e4
CrisRod <crodriguez85@gmail.com>
2019-09-10 10:36:41 -0300

    Rutas

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Componentes
+import AgregarProducto  from './components/AgregarProducto'
+import EditarProducto  from './components/EditarProducto'
+import Productos  from './components/Productos'
+
 function App() {
   return (
     <Router>
       <Switch>
-        
+        <Route exact path="/nuevo-producto" component={AgregarProducto}/>
+        <Route exact path="/productos-editar/:id" component={EditarProducto}/>
+        <Route exact path="/productos" component={Productos}/>
       </Switch>
     </Router>
   );
