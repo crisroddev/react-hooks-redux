2bb4e10f058a4d0760437b25c6fd0b3625cd9721
CrisRod <crodriguez85@gmail.com>
2019-09-26 14:08:33 -0300

    componentes iniciales

---
+++ crud-react/src/components/AgregarProducto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AgregarProducto = () => {
+    return ( 
+        <h1 className="text-center">Desde Agregar Producto</h1>
+     );
+}
+ 
+export default AgregarProducto;
---
+++ crud-react/src/components/Productos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productos = () => {
+    return ( 
+        <h1 className="text-center">Desde Productos</h1>
+     );
+}
+ 
+export default Productos;
