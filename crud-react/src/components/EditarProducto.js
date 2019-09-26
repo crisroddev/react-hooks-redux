3ac5a67941cb6fcf461f71e4931906498de40851
CrisRod <crodriguez85@gmail.com>
2019-09-26 11:12:39 -0300

    delete crud

--- crud-react/src/components/EditarProducto.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EditarProducto = () => {
-    return ( 
-        <h1 className="text-center">Editar Producto</h1>
-     );
-}
- 
-export default EditarProducto;
--- crud-react/src/components/Producto.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Producto = () => {
-    return (
-        <h1 className="text-center">Datos del Producto</h1>
-      );
-}
- 
-export default Producto;
