78fc1525ab8ffc83f2687a054354b1c65741b6d7
CrisRod <crodriguez85@gmail.com>
2019-09-26 14:19:57 -0300

    header

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// Componentes
+import AgregarProducto from './components/AgregarProducto';
+import EditarProducto from './components/EditarProducto';
+import Producto from './components/Producto';
+import Productos from './components/Productos';
+import Header from './components/Header';
+
+
 function App(){
     return(
         <Router>
-            <Switch>
-                <Route/>
-            </Switch>
+            <Header/>
+            <main className="container mt-5">
+                <Switch>
+                    <Route exact path="/productos" component={Productos}/>
+                    <Route exact path="/nuevo-producto" component={AgregarProducto}/>
+                    <Route exact path="/editar-producto/:id" component={EditarProducto}/>
+                    <Route exact path="/producto/:id" component={Producto}/>
+                </Switch>
+            </main>
         </Router>
     );
 }
