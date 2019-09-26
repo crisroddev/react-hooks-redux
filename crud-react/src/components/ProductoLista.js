be017e08690d1946decf2955a45f0f433dc09fc7
CrisRod <crodriguez85@gmail.com>
2019-09-26 15:03:55 -0300

    handleClick para eliminar

--- crud-react/src/components/ProductoLista.js
+++ crud-react/src/components/ProductoLista.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const ProductoLista = ({producto}) => {
+
+    const handleClick = id => {
+        console.log('Eliminando', id)
+    }
+
     return ( 
         <li 
             data-categoria={producto.categoria}
@@ -10,7 +16,17 @@ const ProductoLista = ({producto}) => {
                 <span className="font-weight-bold">${producto.precio}</span>
             </p>
             <div>
-
+                <Link
+                    className="btn btn-success mr-2" 
+                    to={`/editar-producto/${producto.id}`}>
+                    Editar
+                </Link>
+                <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => handleClick(producto.id)}>
+                        Eliminar &times;
+                </button>
             </div>
         </li>
 
