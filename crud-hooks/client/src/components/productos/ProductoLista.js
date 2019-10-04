ff220ae5c1229db281d66d6f90d54de51c77c8d8
CrisRod <crodriguez85@gmail.com>
2019-10-04 13:58:03 -0300

    mostrar productos

---
+++ crud-hooks/client/src/components/productos/ProductoLista.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProductoLista = ({producto}) => {
+    return ( 
+        <li 
+            className="list-group-item d-flex justify-content-between align.items.center"
+            data-categoria={producto.categoria}>
+            <p>
+                {producto.nombre} {' '}
+                <span className="font-weight-bold">$ {producto.precio}</span>
+            </p>
+        </li>
+     );
+}
+ 
+export default ProductoLista;
