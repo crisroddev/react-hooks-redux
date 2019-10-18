5ab04b94045e72d52db93d88507a2b2ab3dab0a0
Cristian <cristian@addshop.app>
2019-10-18 11:15:00 -0300

    localstorage

--- redux/src/store.js
+++ redux/src/store.js
@@ -1,14 +1,22 @@
 import { createStore } from 'redux';
 import reducer from './reducers'
+import { obtenerStateStorage, guardarStateStorage} from './localStorage';
 // Definir state inicial
-const initialState = [];
+// const initialState = [];
+const storageState = obtenerStateStorage();
 
 const store = createStore(
     reducer,
-    initialState,
+    storageState,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__ ()
 
 );
+
+store.subscribe( () => {
+    guardarStateStorage({
+        citas: store.getState().citas
+    })
+})
 
 export default store;
