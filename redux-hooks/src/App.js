1b05f537564a5df09236650572ab8a62922699fc
Cristian <cristian@addshop.app>
2019-10-21 09:43:33 -0300

    routing y componentes

--- redux-hooks/src/App.js
+++ redux-hooks/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
+import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 
 // Redux
 import store from './store';
@@ -6,11 +13,18 @@ import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <p>Crud con Redux</p>
-      </div>
+    <Router>
+      <Provider store={store}>
+        <Header/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Productos}/>
+            <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+            <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+          </Switch>
+        </div>
     </Provider>
+    </Router>
   );
 }
 
