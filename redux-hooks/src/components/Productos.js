2d541c0bc3f2b65f201c8f9daffe961b1e1fd9c6
Cristian <cristian@addshop.app>
2019-10-22 14:28:08 -0300

    if error muestra error

--- redux-hooks/src/components/Productos.js
+++ redux-hooks/src/components/Productos.js
@@ -16,26 +16,33 @@ const Productos = () => {
     }, []);
 
     // Acceder al State
-    const loading = useSelector(state => state.productos.loading)
+    const loading = useSelector(state => state.productos.loading);
+    const error = useSelector(state => state.productos.error);
 
 
     return ( 
         <Fragment>
-            <h2 className="text-center my-5">Listado de Productos</h2>
+            { error 
+                ? <div className="font-weight-bold alert alert-danger text-center mt-4">Hubo un Error</div>
+            : 
+            <Fragment>
+                <h2 className="text-center my-5">Listado de Productos</h2>
 
-            <table className="table table-striped">
-                <thead className="bg-primary table-dark">
-                    <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Precio</th>
-                        <th scope="col">Acciones</th>
-                    </tr>   
-                </thead>
-                <tbody>
+                <table className="table table-striped">
+                    <thead className="bg-primary table-dark">
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Precio</th>
+                            <th scope="col">Acciones</th>
+                        </tr>   
+                    </thead>
+                    <tbody>
 
-                </tbody>
-            </table>
-            { loading ? 'Cargando' : null}
+                    </tbody>
+                </table>
+                { loading ? 'Cargando' : null}
+            </Fragment>
+            }
         </Fragment>
      );
 }
