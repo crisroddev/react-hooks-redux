a99f61be89bc3542065ae71331d8b800ee067127
Cristian <cristian@addshop.app>
2019-10-22 08:41:07 -0300

    productos reducer

---
+++ redux-hooks/src/reducers/productosReducer..js
@@ -0,0 +1,35 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
+
+// Cada Reducer Tiene su State
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        case AGREGAR_PRODUCTO:
+            return {
+                ...state,
+                error: false,
+            }
+        case AGREGAR_PRODUCTO_EXITO:
+            return {
+                ...state,
+                error: false,
+                productos: [...state.productos, action.payload]
+            }
+        case AGREGAR_PRODUCTO_ERROR:
+            return {
+                ...state,
+                error: true
+            }
+        default:
+            return state;
+    }
+}
